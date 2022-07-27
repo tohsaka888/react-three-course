f565ace3530e80846826940826c475c09157f587
tohsaka888 <156132264@qq.com>
2022-07-27 10:15:50 +0800

    ✨ feat(moon): 月亮组件

---
+++ src/components/Moon.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-07-27 10:09:51
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-07-27 10:10:57
+ * @Description: 请填写简介
+ */
+
+import React from 'react'
+
+function Moon() {
+  return (
+    <mesh>
+      <sphereGeometry />
+      <meshPhongMaterial />
+    </mesh>
+  )
+}
+
+export default Moon
