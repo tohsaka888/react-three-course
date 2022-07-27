4551ac4ba0f8a7c5e3c2e1be6f9af3c0875bcebe
tohsaka888 <156132264@qq.com>
2022-07-27 10:30:20 +0800

    🐳 chore: 移除web-vitals

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
+/*
+ * @Author: tohsaka888
+ * @Date: 2022-07-27 08:36:06
+ * @LastEditors: tohsaka888
+ * @LastEditTime: 2022-07-27 10:29:09
+ * @Description: 请填写简介
+ */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,4 +23,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
